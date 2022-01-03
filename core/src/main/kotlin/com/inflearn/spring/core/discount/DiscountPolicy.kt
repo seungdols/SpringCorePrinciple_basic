4f65c6a27bfedfdc4ab168a6c51c0cbc3d5cf845
seungdols <seungdols0822@gmail.com>
2022-01-03 22:55:49 +0900

    Add DiscountPolicy, FixDiscountPolicy

---
+++ core/src/main/kotlin/com/inflearn/spring/core/discount/DiscountPolicy.kt
@@ -0,0 +1,19 @@
+package com.inflearn.spring.core.discount
+
+import com.inflearn.spring.core.member.Member
+
+/**
+ * @PACKAGE com.inflearn.spring.core.discount
+ * @Author seungdols
+ * @Date 2022/01/03
+ */
+interface DiscountPolicy {
+
+  /**
+   *
+   * @param member
+   * @param price
+   * @return 할인 대상 금액
+   */
+  fun discount(member: Member, price: Int): Int
+}
---
+++ core/src/main/kotlin/com/inflearn/spring/core/discount/FixDiscountPolicy.kt
@@ -0,0 +1,22 @@
+package com.inflearn.spring.core.discount
+
+import com.inflearn.spring.core.member.Grade
+import com.inflearn.spring.core.member.Member
+
+/**
+ * @PACKAGE com.inflearn.spring.core.discount
+ * @Author seungdols
+ * @Date 2022/01/03
+ */
+class FixDiscountPolicy : DiscountPolicy {
+
+  private val discountFixAmount = 1000
+
+  override fun discount(member: Member, price: Int): Int {
+    return if (member.grade == Grade.VIP) {
+      discountFixAmount
+    } else {
+      0
+    }
+  }
+}
