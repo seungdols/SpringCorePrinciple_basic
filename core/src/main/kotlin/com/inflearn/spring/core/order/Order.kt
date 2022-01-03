b92b33e44701f1fba5711defee6dd3d5857459ee
seungdols <seungdols0822@gmail.com>
2022-01-03 23:05:11 +0900

    Add Order, OrderService

---
+++ core/src/main/kotlin/com/inflearn/spring/core/order/Order.kt
@@ -0,0 +1,17 @@
+package com.inflearn.spring.core.order
+
+/**
+ * @PACKAGE com.inflearn.spring.core.order
+ * @Author seungdols
+ * @Date 2022/01/03
+ */
+class Order(val memberId: Long, val itemName: String, val itemPrice: Int, val discountPrice: Int) {
+
+  fun caclulatePrice(): Int {
+    return itemPrice - discountPrice
+  }
+
+  override fun toString(): String {
+    return "Order(memberId=$memberId, itemName='$itemName', itemPrice=$itemPrice, discountPrice=$discountPrice)"
+  }
+}
