1023e0e83d3670794167975370cba2ea34dfcc05
seungdols <seungdols0822@gmail.com>
2022-01-03 21:53:44 +0900

    Add MemberService test

---
+++ core/src/test/kotlin/com/inflearn/spring/core/member/MemberServiceImplTest.kt
@@ -0,0 +1,27 @@
+package com.inflearn.spring.core.member
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+/**
+ * @PACKAGE com.inflearn.spring.core.member
+ * @Author seungdols
+ * @Date 2022/01/03
+ */
+internal class MemberServiceImplTest {
+
+  val memberService = MemberServiceImpl()
+
+  @Test
+  fun joinTest() {
+    //given
+    val member = Member(1L, "memberA", Grade.VIP)
+
+    //when
+    memberService.join(member)
+    val findMember = memberService.findMember(1L)
+
+    assertThat(member).isEqualTo(findMember)
+  }
+}
