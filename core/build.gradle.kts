f3249d6edeebedd423d82c7f04fba35d4fb3c39a
seungdols <seungdols0822@gmail.com>
2022-01-05 23:45:43 +0900

    Modify lint plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jmailen.kotlinter") version "3.8.0"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
 }
