f82ccd98170e26271d8104dcd3f3f74ab375ad10
seungdols <seungdols0822@gmail.com>
2022-01-06 23:52:40 +0900

    Modify OrderSerivceImpl

--- core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
@@ -1,16 +1,13 @@
 package com.inflearn.spring.core.order
 
 import com.inflearn.spring.core.discount.DiscountPolicy
-import com.inflearn.spring.core.discount.RateDiscountPolicy
 import com.inflearn.spring.core.member.MemberRepository
 import com.inflearn.spring.core.member.MemoryMemberRepository
 
-class OrderServiceImpl : OrderService {
+// discountPolicy의 구현체를 어디선가에서 넣어줘야 된다.
+class OrderServiceImpl(private val discountPolicy: DiscountPolicy) : OrderService {
 
     private val memberRepository: MemberRepository = MemoryMemberRepository()
-//    private val discountPolicy: DiscountPolicy = FixDiscountPolicy()
-    // 요구사항에 대해 변경이 필요해서 바꾸자니, OCP를 위반함. 역할이 구현체까지 의존하는 상태
-    private val discountPolicy: DiscountPolicy = RateDiscountPolicy()
 
     override fun createOrder(memberId: Long, itemName: String, itemPrice: Int): Order {
         val member = memberRepository.findById(memberId)
