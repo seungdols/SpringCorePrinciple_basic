f0b7900cc85bdee3d299cf2a647c9d6f27503907
seungdols <seungdols0822@gmail.com>
2022-01-07 23:16:28 +0900

    Modify parameter type of discount function

--- core/src/main/kotlin/com/inflearn/spring/core/discount/RateDiscountPolicy.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/discount/RateDiscountPolicy.kt
@@ -7,11 +7,7 @@ class RateDiscountPolicy : DiscountPolicy {
 
     private val discountPercent = 10
 
-    override fun discount(member: Member?, price: Int): Int {
-        if (member == null) {
-            return 0
-        }
-
+    override fun discount(member: Member, price: Int): Int {
         return if (member.grade == Grade.VIP) {
             price * discountPercent / 100
         } else {
