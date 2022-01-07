472edad7407b7dd384afdcfe99f058ea902dcc6f
seungdols <seungdols0822@gmail.com>
2022-01-07 23:00:58 +0900

    Use primary constructor

--- core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
@@ -2,12 +2,12 @@ package com.inflearn.spring.core.order
 
 import com.inflearn.spring.core.discount.DiscountPolicy
 import com.inflearn.spring.core.member.MemberRepository
-import com.inflearn.spring.core.member.MemoryMemberRepository
 
 // discountPolicy의 구현체를 어디선가에서 넣어줘야 된다.
-class OrderServiceImpl(private val discountPolicy: DiscountPolicy) : OrderService {
-
-    private val memberRepository: MemberRepository = MemoryMemberRepository()
+class OrderServiceImpl(
+    private val memberRepository: MemberRepository,
+    private val discountPolicy: DiscountPolicy
+) : OrderService {
 
     override fun createOrder(memberId: Long, itemName: String, itemPrice: Int): Order {
         val member = memberRepository.findById(memberId)
