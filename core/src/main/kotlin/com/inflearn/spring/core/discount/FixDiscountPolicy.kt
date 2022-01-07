6c36707b696a97ead83741aa2f9c04f58999fea3
seungdols <seungdols0822@gmail.com>
2022-01-07 23:16:28 +0900

    Modify parameter type of discount function

--- core/src/main/kotlin/com/inflearn/spring/core/discount/FixDiscountPolicy.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/discount/FixDiscountPolicy.kt
@@ -12,12 +12,7 @@ class FixDiscountPolicy : DiscountPolicy {
 
     private val discountFixAmount = 1000
 
-    override fun discount(member: Member?, price: Int): Int {
-        // 우선 null check를 넣어 준다. 깔끔하게 뺄 수 있는 방법 고려
-        if (member == null) {
-            return 0
-        }
-
+    override fun discount(member: Member, price: Int): Int {
         return if (member.grade == Grade.VIP) {
             discountFixAmount
         } else {
