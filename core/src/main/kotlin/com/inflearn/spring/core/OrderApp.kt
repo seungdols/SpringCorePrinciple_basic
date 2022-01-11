5cc882d57d49f8e1e2676fdbc3031d13a2fdfcf5
seungdols <seungdols0822@gmail.com>
2022-01-11 22:11:01 +0900

    Add orderService main code

---
+++ core/src/main/kotlin/com/inflearn/spring/core/OrderApp.kt
@@ -0,0 +1,23 @@
+package com.inflearn.spring.core
+
+import com.inflearn.spring.core.member.Grade
+import com.inflearn.spring.core.member.Member
+import com.inflearn.spring.core.member.MemberService
+import com.inflearn.spring.core.order.Order
+import com.inflearn.spring.core.order.OrderService
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+fun main() {
+
+    val applicationContext: ApplicationContext = AnnotationConfigApplicationContext(AppConfig::class.java)
+    val memberService: MemberService = applicationContext.getBean("memberService") as MemberService
+
+    val orderService: OrderService = applicationContext.getBean("orderService") as OrderService
+
+    val member = Member(1L, "memberA", Grade.VIP)
+    memberService.join(member)
+
+    val order: Order = orderService.createOrder(member.id, "itemA", 10000)
+    println(order)
+}
