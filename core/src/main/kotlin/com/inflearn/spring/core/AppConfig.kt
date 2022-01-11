53d672c78a4155fa3cefd2637a61c62a817aa33d
seungdols <seungdols0822@gmail.com>
2022-01-11 22:06:01 +0900

    Apply spring framework

--- core/src/main/kotlin/com/inflearn/spring/core/AppConfig.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/AppConfig.kt
@@ -7,14 +7,21 @@ import com.inflearn.spring.core.member.MemberServiceImpl
 import com.inflearn.spring.core.member.MemoryMemberRepository
 import com.inflearn.spring.core.order.OrderService
 import com.inflearn.spring.core.order.OrderServiceImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class AppConfig {
 
-    private fun memoryMemberRepository() = MemoryMemberRepository()
+    @Bean
+    fun memoryMemberRepository() = MemoryMemberRepository()
 
+    @Bean
     fun memberService(): MemberService = MemberServiceImpl(memoryMemberRepository())
 
+    @Bean
     fun orderService(): OrderService = OrderServiceImpl(memoryMemberRepository(), discoutPolicy())
 
+    @Bean
     fun discoutPolicy(): DiscountPolicy = RateDiscountPolicy()
 }
