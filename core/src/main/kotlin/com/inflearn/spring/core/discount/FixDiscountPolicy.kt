dadbde3ca16520dd1f1533bdd90057ea98cb9d22
seungdols <seungdols0822@gmail.com>
2022-03-10 21:27:15 +0900

    Modify FixDiscountPolicy, OrderService

--- core/src/main/kotlin/com/inflearn/spring/core/discount/FixDiscountPolicy.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/discount/FixDiscountPolicy.kt
@@ -2,12 +2,14 @@ package com.inflearn.spring.core.discount
 
 import com.inflearn.spring.core.member.Grade
 import com.inflearn.spring.core.member.Member
+import org.springframework.stereotype.Component
 
 /**
  * @PACKAGE com.inflearn.spring.core.discount
  * @Author seungdols
  * @Date 2022/01/03
  */
+@Component
 class FixDiscountPolicy : DiscountPolicy {
 
     private val discountFixAmount = 1000
--- core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
+++ core/src/main/kotlin/com/inflearn/spring/core/order/OrderServiceImpl.kt
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Component
 @Component
 class OrderServiceImpl(
     private val memberRepository: MemberRepository,
-    private val discountPolicy: DiscountPolicy
+    private val rateDiscountPolicy: DiscountPolicy
 ) : OrderService {
 
     override fun createOrder(memberId: Long, itemName: String, itemPrice: Int): Order {
         val member = memberRepository.findById(memberId)
-        val discountPrice = discountPolicy.discount(member, itemPrice)
+        val discountPrice = rateDiscountPolicy.discount(member, itemPrice)
 
         return Order(memberId, itemName, itemPrice, discountPrice)
     }
